73ffbd140d09763ea24afe76f85c370758173ec4
caowei <caowei@zenmen.com>
2018-05-10 22:03:57 +0800

    side view

--- PocketReader-Android/app/src/main/java/com/pocketreader/pocketreader/BaseActivity.java
+++ PocketReader-Android/app/src/main/java/com/pocketreader/pocketreader/BaseActivity.java
@@ -1,9 +1,11 @@
 package com.pocketreader.pocketreader;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.WindowManager;
 
 /**
  * Created by tony on 5/7/18.
@@ -13,10 +15,22 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setWindowStatus();
+    }
+
+    // 设置状态栏
+    protected void setWindowStatus() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // 透明状态栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            // 透明导航栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            // 设置状态栏颜色
+            getWindow().setBackgroundDrawableResource(R.color.main_color);
+        }
     }
 
     protected void initToolbar(Toolbar toolbar) {
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 }
--- PocketReader-Android/app/src/main/java/com/pocketreader/pocketreader/MainActivity.java
+++ PocketReader-Android/app/src/main/java/com/pocketreader/pocketreader/MainActivity.java
@@ -6,16 +6,34 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.pocketreader.pocketreader.account.AccountUtils;
 import com.pocketreader.pocketreader.fragment.BaseFragment;
-import com.pocketreader.pocketreader.fragment.LinkFragment;
 import com.pocketreader.pocketreader.fragment.HotFragment;
+import com.pocketreader.pocketreader.fragment.LinkFragment;
 import com.pocketreader.pocketreader.fragment.MineFragment;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class MainActivity extends BaseActivity {
+    @BindView(R.id.ic_portrait)
+    CircleImageView icPortrait;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+    @BindView(R.id.ic_search)
+    SearchView icSearch;
+
     private LinkFragment mLinkFragment;
     private HotFragment mHotFragment;
     private MineFragment mMineFragment;
@@ -29,6 +47,7 @@ public class MainActivity extends BaseActivity {
             finish();
         }
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         initToolbar();
         initViews();
     }
@@ -65,6 +84,7 @@ public class MainActivity extends BaseActivity {
                 .commit();
     }
 
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -82,4 +102,25 @@ public class MainActivity extends BaseActivity {
             return false;
         }
     };
+
+    @OnClick({R.id.ic_portrait, R.id.drawer_layout, R.id.ic_search})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.ic_portrait:
+                drawerLayout.openDrawer(Gravity.START);
+                break;
+            case R.id.ic_search:
+                Toast.makeText(this, "search", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(Gravity.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
